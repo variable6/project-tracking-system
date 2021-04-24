8c29ac2a7747fd2d928ad2058d8244db28900e76
Pranay kumar <61114507+pranaydarmapuri@users.noreply.github.com>
2021-04-24 19:43:26 +0530

    version-1.24.19.43

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 //  ---->>>>>>>>>>>>> static keys
-const version = 1.1
+const version = 1.2
 const CACHE_NAME = `site-assets-${version}`
 
 const dbURL = 'https://pts-001.herokuapp.com/api/'
@@ -9,8 +9,8 @@ const assets = [
   '/manifest.json',
   '/favicon.png',
   'https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&family=Open+Sans:wght@300;400;600;700;800&display=swap',
-  `/static/js/2.2a4f905d.chunk.js`,
-  '/static/js/main.7dc35c2a.chunk.js',
+  `/static/js/2.b1797243.chunk.js`,
+  '/static/js/main.7cd7c1a6.chunk.js',
   '/static/js/3.35ab7edc.chunk.js ',
   '/static/js/runtime-main.278b9ea6.js',
   '/static/css/main.3e6a0a69.chunk.css'
